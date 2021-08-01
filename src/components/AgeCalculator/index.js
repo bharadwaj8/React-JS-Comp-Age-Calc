13d7b3578b5ce856aef6c11908dfabb3362d2567
Bharadwaju adari <adaribharadwaj@gmail.com>
2021-08-01 11:27:31 +0530

    Update index.js

--- src/components/AgeCalculator/index.js
+++ src/components/AgeCalculator/index.js
@@ -1,32 +1,58 @@
+// Write your code here.
 import './index.css'
 import {Component} from 'react'
 
 class AgeCalculator extends Component {
-  state = {year: 0}
+  state = {year: 0, error: 0, count: 0}
 
   getInput = e => {
-    console.log(e.target.value)
     this.setState({year: e.target.value})
   }
 
-  render() {
+  validateInput = () => {
     const {year} = this.state
-
     let check = 0
-    const validateInput = () => {
-      // console.log(typeof year)
-      console.log(typeof parseInt(year, 10))
-      if (year < 2021) {
-        check = year - 2021
-      }
+    let error = 0
+    const l = year.length
+    if (l <= 4 && year < '2021') {
+      check = 2021 - year
+    } else {
+      error = 1
     }
+    this.setState({error, count: check})
+  }
 
+  render() {
+    const {error, count} = this.state
+
+    const active = count > 1 ? 's' : ''
+
+    console.log(error)
     return (
       <div className="bg">
         <div className="container">
           <h1 className="heading">Age Calculator</h1>
-          <input type="text" className="input" onChange={this.getInput} />
-          <button className="button" type="button" onClick={validateInput}>
+          <div>
+            <input
+              type="text"
+              className="input"
+              placeholder="Enter the year that you are Born"
+              onChange={this.getInput}
+            />
+            {error ? (
+              <p className="invalid">Enter the year that you are Born</p>
+            ) : (
+              ''
+            )}
+          </div>
+          {count ? (
+            <p className="valid">
+              You are {count} year{active} old by the end of 2021
+            </p>
+          ) : (
+            ''
+          )}
+          <button className="button" type="button" onClick={this.validateInput}>
             Calculate
           </button>
         </div>
